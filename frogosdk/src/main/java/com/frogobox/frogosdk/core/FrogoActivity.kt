1d4087e52799af4ac921e6c63702755e0f9720ab
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-07-27 15:56:19 +0700

    add: Piracy checker

--- frogosdk/src/main/java/com/frogobox/frogosdk/core/FrogoActivity.kt
+++ frogosdk/src/main/java/com/frogobox/frogosdk/core/FrogoActivity.kt
@@ -2,13 +2,20 @@ package com.frogobox.frogosdk.core
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.frogobox.frogolog.FLog
+import com.github.javiersantos.piracychecker.*
+import com.github.javiersantos.piracychecker.enums.Display
+import com.github.javiersantos.piracychecker.enums.InstallerID
+import com.github.javiersantos.piracychecker.utils.apkSignatures
 import com.google.gson.Gson
 
 /*
@@ -26,6 +33,8 @@ import com.google.gson.Gson
 abstract class FrogoActivity<VB : ViewBinding> : AppCompatActivity(), IFrogoActivity {
 
     protected val frogoActivity by lazy { this }
+
+    protected var piracyCheckerDisplay = Display.DIALOG
 
     protected lateinit var binding: VB
 
@@ -122,6 +131,85 @@ abstract class FrogoActivity<VB : ViewBinding> : AppCompatActivity(), IFrogoActi
     protected inline fun <reified Model> baseGetExtraData(extraKey: String): Model {
         val extraIntent = intent.getStringExtra(extraKey)
         return Gson().fromJson(extraIntent, Model::class.java)
+    }
+
+    override fun verifySignature() {
+        piracyChecker {
+            display(piracyCheckerDisplay)
+            enableSigningCertificates("478yYkKAQF+KST8y4ATKvHkYibo=") // Wrong signature
+            //enableSigningCertificates("VHZs2aiTBiap/F+AYhYeppy0aF0=") // Right signature
+        }.start()
+    }
+
+    override fun readSignature() {
+        val dialogMessage = StringBuilder()
+        apkSignatures.forEach {
+            Log.e("Signature", it)
+            dialogMessage.append("* ").append(it).append("\n")
+        }
+        AlertDialog.Builder(this)
+            .setTitle("APK")
+            .setMessage(dialogMessage.toString())
+            .show()
+    }
+
+    override fun verifyInstallerId() {
+        piracyChecker {
+            display(piracyCheckerDisplay)
+            enableInstallerId(InstallerID.GOOGLE_PLAY)
+        }.start()
+    }
+
+    override fun verifyUnauthorizedApps() {
+        piracyChecker {
+            display(piracyCheckerDisplay)
+            enableUnauthorizedAppsCheck()
+            //blockIfUnauthorizedAppUninstalled("license_checker", "block")
+        }.start()
+    }
+
+    override fun verifyStores() {
+        piracyChecker {
+            display(piracyCheckerDisplay)
+            enableStoresCheck()
+        }.start()
+    }
+
+    override fun verifyDebug() {
+        piracyChecker {
+            display(piracyCheckerDisplay)
+            enableDebugCheck()
+            callback {
+                allow {
+                    // Do something when the user is allowed to use the app
+                }
+                doNotAllow { piracyCheckerError, pirateApp ->
+                    // You can either do something specific when the user is not allowed to use the app
+                    // Or manage the error, using the 'error' parameter, yourself (Check errors at {@link PiracyCheckerError}).
+
+                    // Additionally, if you enabled the check of pirate apps and/or third-party stores, the 'app' param
+                    // is the app that has been detected on device. App can be null, and when null, it means no pirate app or store was found,
+                    // or you disabled the check for those apps.
+                    // This allows you to let users know the possible reasons why license is been invalid.
+                }
+                onError { error ->
+                    // This method is not required to be implemented/overriden but...
+                    // You can either do something specific when an error occurs while checking the license,
+                    // Or manage the error, using the 'error' parameter, yourself (Check errors at {@link PiracyCheckerError}).
+                }
+            }
+        }.start()
+    }
+
+    override fun verifyEmulator() {
+        piracyChecker {
+            display(piracyCheckerDisplay)
+            enableEmulatorCheck(false)
+        }.start()
+    }
+
+    override fun showApkSignatures() {
+        apkSignatures.forEach { FLog.d("Signature This Apps : $it") }
     }
 
 }
--- frogosdk/src/main/java/com/frogobox/frogosdk/core/IFrogoActivity.kt
+++ frogosdk/src/main/java/com/frogobox/frogosdk/core/IFrogoActivity.kt
@@ -1,7 +1,13 @@
 package com.frogobox.frogosdk.core
 
+import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.frogobox.frogolog.FLog
+import com.github.javiersantos.piracychecker.*
+import com.github.javiersantos.piracychecker.enums.InstallerID
+import com.github.javiersantos.piracychecker.utils.apkSignatures
 
 /*
  * Created by faisalamir on 26/07/21
@@ -34,5 +40,21 @@ interface IFrogoActivity {
         argumentKey: String,
         extraDataResult: Model
     )
+
+    fun verifySignature()
+
+    fun readSignature()
+
+    fun verifyInstallerId()
+
+    fun verifyUnauthorizedApps()
+
+    fun verifyStores()
+
+    fun verifyDebug()
+
+    fun verifyEmulator()
+
+    fun showApkSignatures()
 
 }
